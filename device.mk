69fbe940e3f8654d9bfc3fd58ce3210e16785614
erfanoabdi <erfangplus@gmail.com>
2022-01-18 09:15:45 +0530

    vince: Prepare to move to in-tree HALs
    Signed-off-by: andro-sapien <alanmadathil007@gmail.com>
    Change-Id: Ia57737c36a55f7c14898afdd60aa7ce738925779

--- device.mk
+++ device.mk
@@ -40,6 +40,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+QCOM_SOONG_NAMESPACE := \
+    $(LOCAL_PATH)/qcom-caf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
