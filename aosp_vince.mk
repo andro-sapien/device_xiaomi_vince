af8eb8d7f086ead74a7c04033b5208f802b97fd7
andro-sapien <alanmadathil007@gmail.com>
2022-02-18 20:45:51 +0530

    vince: Bump up fingerprint to SQ1A.220205.002 8010174
    Signed-off-by: andro-sapien <alanmadathil007@gmail.com>

--- aosp_vince.mk
+++ aosp_vince.mk
@@ -47,7 +47,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220205.002 8010174 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
- BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220205.002/8010174:user/release-keys"
